ade485fc86ab07d856c4f028c687027d1297b7fe
Pavel <Pavel.Vlasov@nasdanika.org>
2023-12-12 00:22:32 -0500

    Resolution of cross-references using registry and elementProvider

--- model/src/main/java/org/nasdanika/models/excel/util/MappingWorkbookResourceFactory.java
+++ model/src/main/java/org/nasdanika/models/excel/util/MappingWorkbookResourceFactory.java
@@ -1,10 +1,14 @@
 package org.nasdanika.models.excel.util;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
@@ -38,11 +42,36 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 
 	protected void loadContents(Workbook workbook, Resource resource) {
 		EObject root = createRoot();
+		
+		record RegistryRecord(Row row, EObject semanticElement) {}
+		Collection<RegistryRecord> registryRecords = new ArrayList<>();
+		
+		record PredicateRecord(BiPredicate<Row,EObject> predicate, Consumer<EObject> consumer) {}		
+		Collection<PredicateRecord> predicateRecords = new ArrayList<>();
+		
+		BiConsumer<Row, EObject> registry = (row, semanticElement) -> {
+			for (PredicateRecord predicateRecord: predicateRecords) {
+				if (predicateRecord.predicate().test(row, semanticElement)) {
+					predicateRecord.consumer().accept(semanticElement);
+				}
+			}
+			registryRecords.add(new RegistryRecord(row, semanticElement));
+		};
+		
+		BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider = (predicate, consumer) -> {
+			for (RegistryRecord registryRecord: registryRecords) {
+				if (predicate.test(registryRecord.row(), registryRecord.semanticElement())) {
+					consumer.accept(registryRecord.semanticElement());
+				}
+				predicateRecords.add(new PredicateRecord(predicate, consumer));
+			}
+		};		
+		
 		for (Sheet sheet: workbook.getSheets()) {
 			if (isFeatureSheet(sheet, resource)) {
 				for (EStructuralFeature rootFeature: root.eClass().getEAllStructuralFeatures()) {
 					if (isFeatureSheet(sheet, resource, rootFeature)) {
-						loadFeature(root, rootFeature, sheet, resource);
+						loadFeature(root, rootFeature, sheet, resource, registry, elementProvider);
 					}					
 				}
 			} else {
@@ -50,7 +79,7 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 					if (isFeatureRow(row, resource)) {
 						for (EStructuralFeature rootFeature: root.eClass().getEAllStructuralFeatures()) {
 							if (isFeatureRow(row, resource, rootFeature)) {
-								loadFeature(root, rootFeature, row, resource);
+								loadFeature(root, rootFeature, row, resource, elementProvider);
 							}					
 						}
 					} 					
@@ -68,13 +97,28 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 	 * @param rootFeature
 	 * @param sheet
 	 * @param resource
+	 * @param registry Takes created row objects to match with predicates and pass to consumers
+	 * @param elementProvider Use for resolving cross-references - pass a {@link BiPredicate} for an {@link EObject} created from a {@link Row} and a consumer of that 
+	 * EObject. For example, if "citi" row references a "country" row in another sheet then the predicate may check for country name or code
 	 */
-	protected void loadFeature(EObject root, EStructuralFeature rootFeature, Sheet sheet, Resource resource) {
+	protected void loadFeature(
+			EObject root, 
+			EStructuralFeature rootFeature, 
+			Sheet sheet, 
+			Resource resource,
+			BiConsumer<Row, EObject> registry,
+			BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider) {
 		if (sheet instanceof RowSheet) {
 			RowSheet rowSheet = (RowSheet) sheet;
 			for (Row row: rowSheet.getRows()) {
 				if (row.getNumber() > 0) {
-					loadFeatureElement(root, rootFeature, row, resource);
+					loadFeatureElement(
+							root, 
+							rootFeature, 
+							row, 
+							resource,
+							registry,
+							elementProvider);
 				}
 			}
 		}
@@ -90,18 +134,24 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
-	protected void loadFeatureElement(EObject root, EStructuralFeature rootFeature, Row row, Resource resource) {
+	protected void loadFeatureElement(
+			EObject root, 
+			EStructuralFeature rootFeature, 
+			Row row, 
+			Resource resource,
+			BiConsumer<Row, EObject> registry,
+			BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider) {
 		if (row instanceof CellRow) {
 			CellRow cellRow = (CellRow) row;
 			if (rootFeature instanceof EReference) {
 				EReference rootReference = (EReference) rootFeature;
-				EObject referenceElement = createReferenceElement(rootFeature, rootReference, cellRow, resource);
+				EObject referenceElement = createReferenceElement(rootFeature, rootReference, cellRow, resource, elementProvider);
 				if (referenceElement != null) {
 					// Loading features from cells
 					for (Cell cell: cellRow.getCells()) {
 						for (EStructuralFeature elementFeature: referenceElement.eClass().getEAllStructuralFeatures()) {
 							if (isFeatureCell(cell, resource, elementFeature)) {
-								loadFeature(referenceElement, elementFeature, cell, resource);
+								loadFeature(referenceElement, elementFeature, cell, resource, elementProvider);
 							}					
 						}						
 					}
@@ -111,9 +161,11 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 					} else {
 						root.eSet(rootReference, referenceElement);
 					}
+					
+					registry.accept(cellRow, referenceElement);					
 				}
 			} else 	if (!cellRow.getCells().isEmpty()) {
-				loadFeature(root, rootFeature, cellRow.getCells().get(0), resource);
+				loadFeature(root, rootFeature, cellRow.getCells().get(0), resource, elementProvider);
 			}			
 		}
 	}
@@ -146,7 +198,12 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 	 * @param resource
 	 * @return
 	 */
-	protected EObject createReferenceElement(EObject root, EReference rootReference, Row row, Resource resource) {
+	protected EObject createReferenceElement(
+			EObject root, 
+			EReference rootReference, 
+			Row row, 
+			Resource resource,
+			BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider) {
 		EClass type = rootReference.getEReferenceType();
 		return type.getEPackage().getEFactoryInstance().create(type);
 	}
@@ -215,8 +272,19 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 	 * @param row
 	 * @param resource
 	 */
-	protected void loadFeature(EObject root, EStructuralFeature rootFeature, Row row, Resource resource) {
-		loadFeature(root, rootFeature, ((CellRow) row).getCells().get(1), resource);		
+	protected void loadFeature(
+			EObject root, 
+			EStructuralFeature 
+			rootFeature, 
+			Row row, 
+			Resource resource,
+			BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider) {
+		loadFeature(
+				root, 
+				rootFeature, 
+				((CellRow) row).getCells().get(1), 
+				resource,
+				elementProvider);		
 	}
 	
 	/**
@@ -226,20 +294,30 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 	 * @param cell
 	 * @param resource
 	 */
-	protected void loadFeature(EObject root, EStructuralFeature feature, Cell cell, Resource resource) {
+	protected void loadFeature(
+			EObject root, 
+			EStructuralFeature feature, 
+			Cell cell, 
+			Resource resource,
+			BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider) {
 		if (cell instanceof BooleanCell) {
-			loadFeatureValue(root, feature, ((BooleanCell) cell).isValue(), resource);
+			loadFeatureValue(root, feature, ((BooleanCell) cell).isValue(), resource, elementProvider);
 		} else if (cell instanceof DateCell) {
-			loadFeatureValue(root, feature, ((DateCell) cell).getValue(), resource);
+			loadFeatureValue(root, feature, ((DateCell) cell).getValue(), resource, elementProvider);
 		} else if (cell instanceof NumericCell) {
-			loadFeatureValue(root, feature, ((NumericCell) cell).getValue(), resource);
+			loadFeatureValue(root, feature, ((NumericCell) cell).getValue(), resource, elementProvider);
 		} else if (cell instanceof StringCell) {
-			loadFeatureValue(root, feature, ((StringCell) cell).getValue(), resource);
+			loadFeatureValue(root, feature, ((StringCell) cell).getValue(), resource, elementProvider);
 		}  		
 	}
 	
 	@SuppressWarnings("unchecked")
-	protected void loadFeatureValue(EObject eObj, EStructuralFeature feature, boolean value, Resource resource) {
+	protected void loadFeatureValue(
+			EObject eObj, 
+			EStructuralFeature feature, 
+			boolean value, 
+			Resource resource,
+			BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider) {
 		Class<?> type = feature.getEType().getInstanceClass();
 		if (feature.isMany()) {
 			((Collection<Object>) eObj.eGet(feature)).add(convert(value, type));
@@ -249,7 +327,12 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 	}
 	
 	@SuppressWarnings("unchecked")
-	protected void loadFeatureValue(EObject eObj, EStructuralFeature feature, Date value, Resource resource) {
+	protected void loadFeatureValue(
+			EObject eObj, 
+			EStructuralFeature feature, 
+			Date value, 
+			Resource resource,
+			BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider) {
 		Class<?> type = feature.getEType().getInstanceClass();
 		if (feature.isMany()) {
 			((Collection<Object>) eObj.eGet(feature)).add(convert(value, type));
@@ -259,7 +342,12 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 	}
 	
 	@SuppressWarnings("unchecked")
-	protected void loadFeatureValue(EObject eObj, EStructuralFeature feature, double value, Resource resource) {
+	protected void loadFeatureValue(
+			EObject eObj, 
+			EStructuralFeature feature, 
+			double value, 
+			Resource resource,
+			BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider) {
 		Class<?> type = feature.getEType().getInstanceClass();
 		if (feature.isMany()) {
 			((Collection<Object>) eObj.eGet(feature)).add(convert(value, type));
@@ -269,7 +357,12 @@ public abstract class MappingWorkbookResourceFactory extends WorkbookResourceFac
 	}
 	
 	@SuppressWarnings("unchecked")
-	protected void loadFeatureValue(EObject eObj, EStructuralFeature feature, String value, Resource resource) {
+	protected void loadFeatureValue(
+			EObject eObj, 
+			EStructuralFeature feature, 
+			String value, 
+			Resource resource,
+			BiConsumer<BiPredicate<Row, EObject>, Consumer<EObject>> elementProvider) {
 		Class<?> type = feature.getEType().getInstanceClass();
 		if (feature.isMany()) {
 			((Collection<Object>) eObj.eGet(feature)).add(convert(value, type));
