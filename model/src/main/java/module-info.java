d0a0638890edeacefda4ebb747b432adee60ce0d
pvlasov <Pavel.Vlasov@nasdanika.org>
2024-10-21 00:28:28 -0400

    Cleanup

--- model/src/main/java/module-info.java
+++ model/src/main/java/module-info.java
@@ -12,7 +12,6 @@ module org.nasdanika.models.excel {
 	requires transitive org.eclipse.emf.common;
 	requires transitive org.apache.poi.ooxml;
 	requires org.apache.commons.csv;
-	requires org.nasdanika.common;
 	requires transitive org.nasdanika.exec;
 	
 	provides CapabilityFactory with 
