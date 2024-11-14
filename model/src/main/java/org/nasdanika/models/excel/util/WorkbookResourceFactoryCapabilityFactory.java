7813cf2c3576aa256e9d9269235ed237a0544c90
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2024-11-14 05:53:43 -0500

    2024.11.0

--- model/src/main/java/org/nasdanika/models/excel/util/WorkbookResourceFactoryCapabilityFactory.java
+++ model/src/main/java/org/nasdanika/models/excel/util/WorkbookResourceFactoryCapabilityFactory.java
@@ -2,12 +2,17 @@ package org.nasdanika.models.excel.util;
 
 import org.eclipse.emf.ecore.resource.Resource.Factory;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.nasdanika.capability.CapabilityFactory.Loader;
 import org.nasdanika.capability.emf.ResourceFactoryCapabilityFactory;
+import org.nasdanika.common.ProgressMonitor;
 
 public class WorkbookResourceFactoryCapabilityFactory extends ResourceFactoryCapabilityFactory {
 
 	@Override
-	protected Factory getResourceFactory(ResourceSet resourceSet) {
+	protected Factory getResourceFactory(
+			ResourceSet resourceSet, 
+			Loader loader,
+			ProgressMonitor progressMonitor) {
 		return new WorkbookResourceFactory();
 	}
 	
